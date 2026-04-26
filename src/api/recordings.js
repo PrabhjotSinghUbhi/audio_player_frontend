64329af8ba6e1d47bbf52266e5c130f8d441dad8
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2026-04-26 07:51:23 +0530

    added env for backend url.

--- src/api/recordings.js
+++ src/api/recordings.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://audio-player-v79q.onrender.com'
+   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000'
 });
 
 export const getRecordings = async () => {
